af766a4d788e58e839afc487e47c6ce348b27dd7
Clian Carlos <clian.dev@outlook.com>
2023-04-22 11:57:40 -0300

    fix: Added tests for run formatter

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,39 @@
-import * as assert from 'assert';
+import * as assert from "assert";
+import * as vscode from "vscode";
+import { formatTailwindClasses } from "../../extension";
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+suite("Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Iniciando testes.");
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
-
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test("Formatação de classes do Tailwind", () => {
+    const input = `
+      <div twClasses="w-16 h-16 text-white dark:text-black dark:bg-purple-800 bg-slate-200"></div>
+      <div twClasses="text-purple-300 w-16 h-16 bg-slate-200"></div>
+    `;
+    const expectedOutput = `
+      <div bg="dark:bg-purple-800 bg-slate-200" font="text-white dark:text-black" size="w-16 h-16"></div>
+      <div bg="bg-slate-200" font="text-purple-300" size="w-16 h-16"></div>
+    `;
+    const textDocument = vscode.workspace.createTextDocument({
+      language: "html",
+      content: input,
+    });
+    const edits = formatTailwindClasses(input, textDocument);
+    const workspaceEdit = new vscode.WorkspaceEdit();
+    for (const edit of edits) {
+      workspaceEdit.replace(textDocument.uri, edit.range, edit.newText);
+    }
+    return vscode.workspace.applyEdit(workspaceEdit).then((success) => {
+      assert.strictEqual(
+        success,
+        true,
+        "Falha ao aplicar edições no documento"
+      );
+      assert.strictEqual(
+        textDocument.getText(),
+        expectedOutput,
+        "Saída incorreta"
+      );
+    });
+  });
 });
